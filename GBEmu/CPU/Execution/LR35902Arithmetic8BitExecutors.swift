1d5beaa084535916516abb20c7184432041bd24d
Keahi <devkeahi@gmail.com>
2023-04-29 10:19:23 -0400

    Rename `LR35902ExecutionError` to `CPUError`

--- GBEmu/CPU/Execution/LR35902Arithmetic8BitExecutors.swift
+++ GBEmu/CPU/Execution/LR35902Arithmetic8BitExecutors.swift
@@ -11,43 +11,43 @@ extension CPU {
     /// 0x04 INC B
     @discardableResult
     func inc_B() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x0C INC C
     @discardableResult
     func inc_C() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x14 INC D
     @discardableResult
     func inc_D() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x1C INC E
     @discardableResult
     func inc_E() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x24 INC H
     @discardableResult
     func inc_H() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x2C INC L
     @discardableResult
     func inc_L() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x3C INC A
     @discardableResult
     func inc_A() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 }
 
--- GBEmu/CPU/Execution/LR35902ControlInstructionExecutors.swift
+++ GBEmu/CPU/Execution/LR35902ControlInstructionExecutors.swift
@@ -13,54 +13,54 @@ extension CPU {
     /// 0x00
     @discardableResult
     func nop() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x10
     @discardableResult
     func stop() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x27
     @discardableResult
     func daa() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x2F
     @discardableResult
     func cpl() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x37
     @discardableResult
     func scf() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x3F
     @discardableResult
     func ccf() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0x76
     @discardableResult
     func halt() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0xF3
     @discardableResult
     func di() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 
     /// 0xFB
     @discardableResult
     func ei() throws -> LR35902ExecutionResult {
-        throw LR35902ExecutionError.instructionNotImplemented
+        throw CPUError.instructionNotImplemented
     }
 }
